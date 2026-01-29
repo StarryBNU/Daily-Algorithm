e7ba2e848ce6787b7688cfebd3118f1e0ed666ad
Rain1879 <3468067461@qq.com>
2026-01-29 13:56:07 +0800

    A+BProblem

---
+++ P1077.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",a+b);
+    system("pause");
+    return 0;
+}
